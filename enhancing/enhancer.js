86cb99e7c0133a037653a4015c86cd986e62674e
timotheemcdonald <timotheemcdonald@gmail.com>
2020-09-16 18:36:48 -0400

    adding files

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,30 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if(item.enhancement <=19 ){
+    return { ...item, enhancement: (item.enhancement + 1) }
+  } else {
+    return { ...item };
+  }
 }
 
 function fail(item) {
+  if(item.enhancement < 15){
+    return { ...item, durability: (item.durability - 5)}
+  } else if (item.enhancement === 15) {
+    return { ...item, durability: (item.durabiliy -10)}
+  } else if (item.enhancement >16 ) {
+    return {...item, enhancement:(item.enhancement - 1), durability: (item.durability - 10)}
+  } else {
   return { ...item };
 }
+}
+
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
-  return { ...item };
+  return {...item}
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,41 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+
+describe('.repair', () => {
+    it('durability should be 100', function () {
+
+        let item = {
+            name:'test',
+            durability: 2,
+            enhancement: 0
+        }
+
+        const result  = enhancer.repair(item)
+        expect(result.durability).toBe(100)
+    })
+})
+
+describe('.fail', () => {
+    it('durability should be 15', function () {
+        let item = {
+            name:'test2',
+            durability: 20,
+            enhancement: 1
+        }
+        const result = enhancer.fail(item)
+        expect(result.durability).toBe(15)
+    })
+})
+
+describe('.success', () => {
+    it('enhancement should be 11', function () {
+        let item = {
+            name: 'test',
+            durability: 10,
+            enhancement: 10
+        }
+        const result = enhancer.success(item)
+        expect(result.enhancement).toBe(11)
+    })
+})
