291e3377108b9c199a59f10209dabf7352a5111a
timotheemcdonald <timotheemcdonald@gmail.com>
2020-09-16 18:44:12 -0400

    finished

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -17,8 +17,8 @@ function fail(item) {
   if(item.enhancement < 15){
     return { ...item, durability: (item.durability - 5)}
   } else if (item.enhancement === 15) {
-    return { ...item, durability: (item.durabiliy -10)}
-  } else if (item.enhancement >16 ) {
+    return { ...item, durability: (item.durability -10)}
+  } else if (item.enhancement >=16 ) {
     return {...item, enhancement:(item.enhancement - 1), durability: (item.durability - 10)}
   } else {
   return { ...item };
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -26,6 +26,27 @@ describe('.fail', () => {
         const result = enhancer.fail(item)
         expect(result.durability).toBe(15)
     })
+
+    it('durability should be 10', function () {
+        let item = {
+            name: 'test',
+            durability: 20,
+            enhancement: 15
+        }
+        const result = enhancer.fail(item)
+        expect(result.durability).toBe(10)
+    })
+
+    it('durability should be 10 and enhancement should be 15', function () {
+        let item = {
+            name: 'test',
+            durability: 20,
+            enhancement: 16
+        }
+        const result = enhancer.fail(item)
+        expect(result.enhancement).toBe(15)
+        expect(result.durability).toBe(10)
+    })
 })
 
 describe('.success', () => {
